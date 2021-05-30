7522bb6a5834060e14b12fd0c3785c15206324d4
t737070 <linegcruz@gmail.com>
2021-05-30 18:36:06 -0300

    Changed: Configs of Screen Home - Fragment and Activity

--- app/src/main/java/com/alinecruz/devweek/presentation/view/HomeActivity.kt
+++ app/src/main/java/com/alinecruz/devweek/presentation/view/HomeActivity.kt
@@ -3,10 +3,17 @@ package com.alinecruz.devweek.presentation.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alinecruz.devweek.R
+import com.alinecruz.devweek.di.SetupGlobalDependencies
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        //SetupGlobalDependencies(this).init()
+
+
     }
+
+
 }
